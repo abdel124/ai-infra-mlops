05b27cac11825daca62057af00a548aa78a99b5b
abdel124 <touazi.abdelhak@gmail.com>
2025-04-10 16:56:49 -0700

    add OIDC conenctor for github

--- terraform/iam-github.tf
+++ terraform/iam-github.tf
@@ -17,11 +17,41 @@ data "aws_iam_policy_document" "github_oidc_assume_role" {
   }
 }
 
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1" # GitHub's OIDC thumbprint
+  ]
+}
+
 resource "aws_iam_role" "github_actions_deploy" {
   name = "GitHubActionsDeployRole"
 
-  assume_role_policy = data.aws_iam_policy_document.github_oidc_assume_role.json
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:abdel124/ai-infra-mlops:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
 }
+
+
 
 resource "aws_iam_role_policy_attachment" "ecr_access" {
   role       = aws_iam_role.github_actions_deploy.name
