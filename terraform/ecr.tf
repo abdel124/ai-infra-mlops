25b98029705b044a47eee6b2f6ea3143d2d56e54
abdel124 <touazi.abdelhak@gmail.com>
2025-04-10 11:14:21 -0700

    add ecr creation to infra terraform

---
+++ terraform/ecr.tf
@@ -0,0 +1,13 @@
+resource "aws_ecr_repository" "iris_api" {
+  name = "iris-api"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+output "ecr_repo_url" {
+  value = aws_ecr_repository.iris_api.repository_url
+}
